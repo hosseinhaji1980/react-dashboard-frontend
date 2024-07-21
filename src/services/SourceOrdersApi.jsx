35c85dba43c13b1a0de3d21b34a5e990813fd50b
dasborad <hosseinhajimohammadi@gmail.com>
2024-07-21 13:06:48 +0330

    1403-04-31

--- src/services/SourceOrdersApi.jsx
+++ src/services/SourceOrdersApi.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
+require('dotenv').config();
 
+const API_URL = process.env.REACT_APP_API_URL;
 const SourceOrdersApi = {
   getOrdersData: async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/orders/ordersData');
+      const response = await axios.get(`${API_URL}/orders/ordersData`);
       return response.data;
     } catch (error) {
       throw new Error('خطا در دریافت اطلاعات');
